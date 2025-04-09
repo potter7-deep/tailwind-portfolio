be43b51c9741cf4d7f6dc05f38de09433e6407df
Dan <dm549153@gmail.com>
2025-04-09 21:44:27 +0300

    changes-2

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    screen: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        "2xl": "1536px",
-    },
-    color: {
-        primary: '#7DF9FF',
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
