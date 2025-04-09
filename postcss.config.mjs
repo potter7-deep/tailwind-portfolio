7951077f7351affdc6efbdcc51a06acf9e46f5ba
Dan <dm549153@gmail.com>
2025-04-09 21:37:23 +0300

    deploy-changes

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,14 +1,4 @@
 const config = {
-  screen: {
-    sm: "640px",
-    md: "768px",
-    lg: "1024px",
-    xl: "1280px",
-    "2xl": "1536px",
-  },
-  color: {
-    primary: '#7DF9FF',
-  },
   plugins: [
     "@tailwindcss/postcss",
   ],
